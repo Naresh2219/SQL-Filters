30f621f83fd8726d92a06e33cc758d2b1250e8b1
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2025-02-16 18:41:55 +0530

    BIGDARA python

---
+++ naresh.py
@@ -0,0 +1,147 @@
+
+import os
+import urllib.request
+import ssl
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+data_dir1 = "hadoop/bin"
+os.makedirs(data_dir1, exist_ok=True)
+
+urls_and_paths = {
+    "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/master/test.txt": os.path.join(data_dir, "test.txt"),
+    "https://github.com/saiadityaus1/SparkCore1/raw/master/winutils.exe": os.path.join(data_dir1, "winutils.exe"),
+    "https://github.com/saiadityaus1/SparkCore1/raw/master/hadoop.dll": os.path.join(data_dir1, "hadoop.dll")
+}
+
+# Create an unverified SSL context
+ssl_context = ssl._create_unverified_context()
+
+for url, path in urls_and_paths.items():
+    # Use the unverified context with urlopen
+    with urllib.request.urlopen(url, context=ssl_context) as response, open(path, 'wb') as out_file:
+        data = response.read()
+        out_file.write(data)
+import os, urllib.request, ssl; ssl_context = ssl._create_unverified_context(); [open(path, 'wb').write(urllib.request.urlopen(url, context=ssl_context).read()) for url, path in { "https://github.com/saiadityaus1/test1/raw/main/df.csv": "df.csv", "https://github.com/saiadityaus1/test1/raw/main/df1.csv": "df1.csv", "https://github.com/saiadityaus1/test1/raw/main/dt.txt": "dt.txt", "https://github.com/saiadityaus1/test1/raw/main/file1.txt": "file1.txt", "https://github.com/saiadityaus1/test1/raw/main/file2.txt": "file2.txt", "https://github.com/saiadityaus1/test1/raw/main/file3.txt": "file3.txt", "https://github.com/saiadityaus1/test1/raw/main/file4.json": "file4.json", "https://github.com/saiadityaus1/test1/raw/main/file5.parquet": "file5.parquet", "https://github.com/saiadityaus1/test1/raw/main/file6": "file6", "https://github.com/saiadityaus1/test1/raw/main/prod.csv": "prod.csv", "https://raw.githubusercontent.com/saiadityaus1/test1/refs/heads/main/state.txt": "state.txt", "https://github.com/saiadityaus1/test1/raw/main/usdata.csv": "usdata.csv", "https://github.com/saiadityaus1/SparkCore1/raw/refs/heads/master/data.orc": "data.orc", "https://github.com/saiadityaus1/test1/raw/main/usdata.csv": "usdata.csv", "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/refs/heads/master/rm.json": "rm.json"}.items()]
+
+# ======================================================================================
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import sys
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['HADOOP_HOME'] ="hadoop"
+os.environ['JAVA_HOME'] = r'C:\Users\Nares\.jdks\corretto-1.8.0_442'
+######################🔴🔴🔴################################
+
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-avro_2.12:3.5.4 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 pyspark-shell'
+
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+spark.read.format("csv").load("data/test.txt").toDF("Success").show(20, False)
+
+
+##################🔴🔴🔴🔴🔴🔴 -> DONT TOUCH ABOVE CODE -- TYPE BELOW ####################################
+
+print()
+
+
+print("STARTED=============")
+
+lis = [1 , 2 , 3 , 4]
+print(lis)
+rddin = sc.parallelize(lis)
+
+print("===== RAW RDD LIST=====")
+print(rddin.collect())
+
+addrdd = rddin.map( lambda x : x + 2 )
+
+print("===== mulrdd RDD LIST=====")
+print(addrdd.collect())
+
+mulrdd = rddin.map( lambda x : x *  10 )
+
+print("===== mulrdd RDD LIST=====")
+print(mulrdd.collect())
+
+listr = [ "zeyobron" , "zeyo" , "byte" ]
+print()
+print("=======RAW LIST======")
+print (listr)
+
+
+rddstr = sc.parallelize(listr)
+print()
+print("======= RDD LIST=======")
+print(rddstr.collect())
+
+
+conrdd = rddstr.map(lambda  x  :  x + "Analytics")
+print()
+print("======= conrdd LIST=======")
+print(conrdd.collect())
+
+
+
+filrdd = rddstr.filter(  lambda x : "zeyo" in  x)
+print()
+print("======= filrdd LIST=======")
+print(filrdd.collect())
+
+
+#🔴 FILE READ STATES
+
+data = sc.textFile("state.txt")
+print()
+print("====== FILE RDD======")
+print(data.collect())
+data.foreach(print)
+
+
+
+flatten = data.flatMap(lambda x : x.split("~"))
+print()
+print("===== flatten LIST======")
+print(flatten.collect())
+flatten.foreach(print)
+
+
+
+filstate = flatten.filter(lambda x : 'State' in x)
+print()
+print("===== filstate LIST======")
+print(filstate.collect())
+filstate.foreach(print)
+
+
+
+states = filstate.map(lambda x : x.replace("State->",""))
+print()
+print("===== states LIST======")
+print(states.collect())
+states.foreach(print)
+
+
+filcity =  flatten.filter(lambda x : 'City' in x)
+print()
+print("===== filcity LIST======")
+print(filcity.collect())
+filcity.foreach(print)
+
+
+cities = filcity.map(lambda x : x.replace("City->",""))
+print()
+print("===== cities LIST======")
+print(cities.collect())
+cities.foreach(print)
