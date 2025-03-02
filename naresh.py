37a69792cd766c8328211afb658f13040ebde477
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2025-03-02 13:46:08 +0530

    Sql Filters

--- naresh.py
+++ naresh.py
@@ -57,94 +57,67 @@ print()
 # 🔴 ALL MOST ALL FILTER
 
 
-# data = [
-#     ("00000", "06-26-2011", 200, "Exercise", "GymnasticsPro", "cash"),
-#     ("00001", "05-26-2011", 300, "Exercise", "Weightlifting", "credit"),
-#     ("00002", "06-01-2011", 100, "Exercise", "GymnasticsPro", "cash"),
-#     ("00003", "06-05-2011", 100, "Gymnastics", "Rings", "credit"),
-#     ("00004", "12-17-2011", 300, "Team Sports", "Field", "paytm"),
-#     ("00005", "02-14-2011", 200, "Gymnastics", None, "cash"),
-#     ("00006", "03-02-2025", 300, "Food", "Biryani","debit"),
-#     ("00007", "02-15-2025", 500, "Food", "Pizza", "debit"),
-#     ("00008", "01-20-2023", 150, "Clothing", "T-shirt", "credit"),
-#     ("00009", "11-30-2022", 400, "Electronics", "Laptop", "credit"),
-#     ("00010", "07-04-2024", 250, "Entertainment", "Movie Tickets", "debit"),
-#     ("00011", "09-19-2024", 350, "Groceries", "Vegetables", "paytm"),
-#     ("00012", "04-15-2025", 600, "Health", "Supplements", "cash"),
-#     ("00013", "08-10-2022", 150, "Food", "Sandwich", "cash"),
-#     ("00014", "05-15-2023", 250, "Electronics", "Phone", "credit"),
-#     ("00015", "04-10-2023", 600, "Entertainment", "Movie Tickets", "debit"),
-#     ("00016", "06-25-2024", 200, "Groceries", "Vegetables", "credit"),
-#     ("00017", "08-11-2024", 400, "Health", "Vitamins", "debit"),
-#     ("00018", "03-01-2022", 300, "Team Sports", "Football", "cash"),
-#     ("00019", "01-01-2022", 180, "Food", "Burger", "cash"),
-# ]
-df = spark.createDataFrame(data, ["id", "tdate", "amount", "category", "product", "spendby"])
+data4 = [
+    (1, "raj"),
+    (2, "ravi"),
+    (3, "sai"),
+    (5, "rani")
+]
 
-df1 = spark.createDataFrame(data, ["id", "tdate", "amount", "category", "product", "spendby"])
-df1.show()
-df.createOrReplaceTempView("df")
-df1.createOrReplaceTempView("df1")
-#cust.createOrReplaceTempView("cust")
-prod.createOrReplaceTempView("prod")
-#cust.show()
+cust = spark.createDataFrame(data4, ["id", "name"]).coalesce(1)
+cust.show()
+
+data3 = [
+    (1, "mouse"),
+    (3, "mobile"),
+    (7, "laptop")
+]
+
+
+
+prod = spark.createDataFrame(data3, ["id", "product"]).coalesce(1)
 prod.show()
 
-# print("Single cloumn data")
-# spark.sql("select * from df where category = 'Exercise'").show()
-# print("Multi cloumn data")
-# spark.sql("select * from df where category in ('Exercise' ,'Electronics')").show()
-# print("Null data")
-# spark.sql("select * from df where product is null").show()
-# print("Max ID")
-# spark.sql("select max(id) from df").show()
-# print("count")
-# spark.sql("select count(1) from df").show()
-# print("Status of payments")
-# spark.sql("select *, case when spendby = 'cash' then 'green' when spendby = 'paytm' then 'NA' else 'blue' end as status from df").show()
-# print("Concat two columns")
-# spark.sql("select id,category,concat(id,'-',category) from df").show()
-# spark.sql("select id,category,concat(id,'-',category) as condata from df").show()
-# spark.sql("select id,category,concat(id,'-',category,'-',product) as condata from df").show()
-# print("lower case")
-# spark.sql("select category, lower(category) as lower from df").show()
-# print("UPPER CASE")
-# spark.sql("select category, upper(category) as upper from df").show()
-# print("CEIL operation")
-# spark.sql("select amount,ceil(amount) as ceil from df").show()
-# print("Round operation")
-# spark.sql("select amount,round(amount) as round from df").show()
-# print("Replace Nulls")
-# spark.sql("select  product,coalesce(product, 'NA') as nullrep from df").show()
-# print("Trim Space")
-# spark.sql("select trim(product) from df").show()
-# print("Distinct")
-# spark.sql("select distinct category, spendby from df").show()
-# print("Substring")
-# spark.sql("select substring(product,1,10) as sub from df").show()
-# print("Split")
-# spark.sql("select product,split(product,' ')[0] as split from df").show()
-#
-# UNION
-# spark.sql("select * from df union all select * from df1").show()
-#
-# Total amount col for Each catogery
-# spark.sql("select category,sum(amount) as sum from df group by category").show()
-#
-# Total amount category spendby
-# spark.sql("select category,spendby,sum(amount) as sum,count(amount) as count from df group by category,spendby").show()
-#
-#
-# what is MAX amount of every category
-# spark.sql("select category, max(amount) as MAX from df group by category order by category desc").show()
-#
-# Window Row Number
-# spark.sql("SELECT category, amount, densc_rank() OVER (PARTITION BY category ORDER BY amount DESC) AS row_number FROM df").show()
 
-#Window lead function
-#spark.sql("select category, amount, lead(amount) over (partition by category order by amount desc) as lead from df").show()
-#spark.sql("select category, amount, lag(amount) over (partition by category order by amount desc) as lag from df").show()
 
-# Having function
-#spark.sql("select category,count(category) as cnt from df group by category having count(category)>1").show()
-#spark.sql("SELECT a.*,b.product FROM df a join df1 b on a.id=b.id").show()
+
+
+
+
+
+innerjoin = cust.join(prod , ["id"] , "inner")
+
+print("======INNER JOIN======")
+print()
+innerjoin.show()
+
+
+
+left = cust.join(prod, ["id"], "left")
+
+print("======left JOIN======")
+print()
+left.show()
+
+
+right = cust.join(prod, ["id"] , "right")
+
+print("======right JOIN======")
+print()
+right.show()
+
+
+
+full = cust.join(prod, ["id"] , "full" )
+
+print("======full JOIN======")
+print()
+full.show()
+
+
+prod1 = spark.createDataFrame(data3, ["id1", "product"]).coalesce(1)
+prod1.show()
+
+
+inner = cust.join(prod1 ,   cust["id"] == prod1["id1"]    , "inner" ).drop("id1")
+inner.show()
